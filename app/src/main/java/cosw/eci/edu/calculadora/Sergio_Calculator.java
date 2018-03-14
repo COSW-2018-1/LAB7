94e1e8df33a2f5a5910c01e9e7965bbf90ee65d3
Sergio Perez <dark07perez@gmail.com>
2018-03-14 08:25:26 -0500

    Diseño

---
+++ app/src/main/java/cosw/eci/edu/calculadora/Sergio_Calculator.java
@@ -0,0 +1,13 @@
+package cosw.eci.edu.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sergio_Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+    }
+}
